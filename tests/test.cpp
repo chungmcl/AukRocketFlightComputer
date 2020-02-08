987544b111b7ff08f13583621587a0ba61e2fbd7
Micheal Chung <chungmcl@hotmail.com>
2020-02-07 19:29:35 -0800

    stuff

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,14 +5,25 @@ int main(int argc, char* argv[])
 {
 	if (gpioInitialise() >= 0)
 	{
+		//gpioSetMode(23, 1);
+		//gpioSetMode(12, 1);
 		while (1)
 		{        		
-			//gpioSetMode(23, 1);
+			gpioWrite(23, 0);
+			gpioWrite(12, 1);
+			gpioWrite(18, 0);
+			std::cout << "RED";
+			getchar();
 			gpioWrite(23, 1);
-			std::cout << "finish";
+			gpioWrite(12, 0);
+			gpioWrite(18, 0);
+			std::cout<< "GREEN";
 			getchar();
 			gpioWrite(23, 0);
-			getchar();
+			gpioWrite(12, 0);
+			gpioWrite(18, 1);
+			std::cout << "BLUE";
+			getchar();	
 		}
 	}
 }
