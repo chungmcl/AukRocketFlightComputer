57983e4442bb3a6c1243485d6c4c2b3120118cda
chungmcl <chungmcl@hotmail.com>
2020-02-08 22:10:53 -0800

    Working on threading

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,25 +2,46 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <mutex>
 using std::cout;
 
-void Switch(int gpioIdNumber, int milliseconds)
+class GpioWrapper
+{
+public:
+    GpioWrapper(std::mutex* lock)
+    {
+        writeLock = lock;
+    }
+    void GpioWrapper::write(int gpioNumber, int milliseconds)
+    {
+        writeLock->lock();
+        gpioWrite(gpioNumber, milliseconds);
+        writeLock->unlock();
+    }
+private:
+    std::mutex* writeLock;
+};
+
+
+
+void Switch(GpioWrapper* writer, int gpioIdNumber, int milliseconds)
 {
 	while (true)
 	{
-		gpioWrite(gpioIdNumber, 1);
-		//std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
-		gpioWrite(gpioIdNumber, 0);
-		//std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+        (*writer).write(gpioIdNumber, millseconds);
 	}
 }
 
 int main(int argc, char* argv[])
 {
+    std::mutex lock;
+    GpioWrapper redGpioWrapper(&lock);
+    GpioWrapper blueGpioWrapper(&lock);
+    GpioWrapper greenGpioWrapper(&lock);
 	if (gpioInitialise() >= 0)
 	{
-		std::thread redThread(Switch, 12, 1000);
-		std::thread greenThread(Switch, 23, 750);
-		std::thread blueThread(Switch, 18, 500);
+		std::thread redThread(Switch, &redGpioWrapper, 12, 1000);
+		std::thread greenThread(Switch, &blueGpioWrapper, 23, 750);
+		std::thread blueThread(Switch, &greenGpioWrapper, 18, 500);
 	}
 }
