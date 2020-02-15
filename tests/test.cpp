7de0342f430ce76a3d652ffd4ba5808f21c76caf
Micheal Chung <chungmcl@hotmail.com>
2020-02-14 16:37:30 -0800

    Reset some stuff

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,31 +5,11 @@
 #include <mutex>
 using std::cout;
 
-void Switch(int gpioIdNumber, int milliseconds, std::mutex* lock)
-{
-	while (true)
-	{
-		lock->lock();
-		if (gpioInitialise() >= 0)
-		{
-			gpioWrite(gpioIdNumber, 1);
-			std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
-			gpioWrite(gpioIdNumber, 0);
-			std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
-			gpioTerminate();
-		}
-		lock->unlock();
-	}
-}
-
 int main(int argc, char* argv[])
 {
-	std::mutex lock;
-	std::thread redThread(Switch, 12, 1000, &lock);
-	std::thread greenThread(Switch, 23, 750, &lock);
-	std::thread blueThread(Switch, 18, 500, &lock);
+	if (gpioInitialise() >= 0)
+	{
+		i2cOpen();
 		
-	redThread.join();
-	greenThread.join();
-	blueThread.join();
+	}
 }
