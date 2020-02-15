7c72a54832190b3b2a998794b554f9ac53613b9b
Micheal Chung <chungmcl@hotmail.com>
2020-02-14 16:45:35 -0800

    working on i2c

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,15 +1,25 @@
 #include <pigpio.h>
+#include "bmp3.h"
+
 #include <iostream>
 #include <chrono>
 #include <thread>
 #include <mutex>
 using std::cout;
 
+void InitializeAltimeter()
+{
+	struct bmp3_dev dev;
+	int8_t rslt = BMP3_OK;
+	
+	dev.dev_id = BMP3_12C_ADDR_PRIM;
+}
+
 int main(int argc, char* argv[])
 {
 	if (gpioInitialise() >= 0)
 	{
-		i2cOpen();
+		//i2cOpen();
 		
 	}
 }
